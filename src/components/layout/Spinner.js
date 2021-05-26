fa8432dd88a0f58818e32a0f5b36c347d609deb3
Amphakarn <pisuthigomol@yahoo.com>
2021-05-26 19:18:17 +0000

    Fix exporting error for Spinner component

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,14 +1,16 @@
 import React, { Fragment } from 'react';
 import spinner from './spinner.gif';
 
-export default Spinner = () => {
+const Spinner = () => {
   return (
     <Fragment>
       <img
-        src="{spinner}"
+        src={spinner}
         alt="Loading..."
         style={{ width: '200px', margin: 'auto', display: 'block' }}
       />
     </Fragment>
   );
 };
+
+export default Spinner;
