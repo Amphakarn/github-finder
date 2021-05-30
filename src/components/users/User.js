de3d98bbc8ea78dacc5e3172a68e66fc1268a8ba
Amphakarn <pisuthigomol@yahoo.com>
2021-05-30 16:22:48 +0000

    Refactor and clean up User.js for getUserRepos function and repos context

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -2,13 +2,11 @@ import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
 import GithubContext from '../../context/github/githubContext';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const User = ({ repos, getUserRepos, match }) => {
+const User = ({ match }) => {
   const githubContext = useContext(GithubContext);
-  const { user, getUser, loading } = githubContext;
-  console.log('***ghContext: ', githubContext);
+  const { user, getUser, repos, getUserRepos, loading } = githubContext;
 
   useEffect(() => {
     getUser(match.params.login);
@@ -100,11 +98,6 @@ const User = ({ repos, getUserRepos, match }) => {
       <Repos repos={repos} />
     </Fragment>
   );
-};
-
-User.propTypes = {
-  repos: PropTypes.array.isRequired,
-  getUserRepos: PropTypes.func.isRequired,
 };
 
 export default User;
