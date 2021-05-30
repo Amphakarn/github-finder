885a39ad1724e88b2e62b97de117444d157ef00f
Amphakarn <pisuthigomol@yahoo.com>
2021-05-30 15:58:57 +0000

    Set up action for GET_USER type in githubReducer.js

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -14,6 +14,14 @@ const githubReducer = (state, action) => {
         users: action.payload,
         loading: false,
       };
+
+    case GET_USER:
+      return {
+        ...state,
+        user: action.payload,
+        loading: false,
+      };
+
     case CLEAR_USERS:
       return {
         ...state,
