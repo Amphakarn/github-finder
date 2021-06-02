7132bdf59fbd94be0322bd702d628db8b12b9456
Amphakarn <pisuthigomol@yahoo.com>
2021-06-02 17:04:16 +0000

    Clean up

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -104,26 +104,3 @@ const GithubState = (props) => {
 };
 
 export default GithubState;
-
-// import React, { useReducer } from 'react';
-// import GithubContext from './githubContext';
-// import githubReducer from './githubReducer';
-
-// const GithubState = (props) => {
-//   const initialState = {
-//     users: [],
-//     user: {},
-//     repos: [],
-//     loading: false,
-//   };
-
-//   const [state, dispatch] = useReducer(githubReducer, initialState);
-
-//   return (
-//     <GithubContext.Provider value={{ ...state, dispatch }}>
-//       {props.children}
-//     </GithubContext.Provider>
-//   );
-// };
-
-// export default GithubState;
